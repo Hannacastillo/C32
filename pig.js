70318c8603dbea3dbd3c4f499899e8b4648abd3e
Hannacastillo <72773292+Hannacastillo@users.noreply.github.com>
2021-03-10 20:53:24 -0600

    Add files via upload

---
+++ pig.js
@@ -0,0 +1,28 @@
+class Pig extends Parent{
+
+    constructor(x,y){
+ super(x,y,50,50,0)
+this.image=loadImage("sprites/enemy.png")
+    
+this.Vis=255
+
+    }
+    display(){
+//console.log(this.body.speed)
+if (this.body.speed<2){
+    super.display()
+}
+
+    
+    else {
+        World.remove(myworld,this.body)
+        push()
+        this.Vis=this.Vis-5
+        tint(255,this.Vis)
+image(this.image,this.body.position.x,this.body.position.y,50,50)
+  pop()
+}
+    
+   
+    }
+}
